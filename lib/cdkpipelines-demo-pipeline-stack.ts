95359e8bd4142705143c9b34a04e6ae146ba2618
wewang <weihua.wang.contractor@move.com>
2021-08-15 13:58:46 -0700

    Update cdkpipelines-demo-pipeline-stack.ts

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,6 +1,8 @@
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
+import { ShellScriptAction } from '@aws-cdk/pipelines';
+
 /**
  * The stack that defines the application pipeline
  */
@@ -31,5 +33,25 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
       env: { account: '915271087263', region: 'us-west-2' }
     }));
+
+    const preprod = new CdkpipelinesDemoStage(this, 'PreProd', {
+      env: { account: 'ACCOUNT1', region: 'us-east-2' }
+    });
+    const preprodStage = pipeline.addStage(preprod, {
+      post: [
+        new ShellStep('TestService', {
+          commands: [
+            // Use 'curl' to GET the given URL and fail if it returns an error
+            'curl -Ssf $ENDPOINT_URL',
+          ],
+          envFromCfnOutputs: {
+            // Get the stack Output from the Stage and make it available in
+            // the shell script as $ENDPOINT_URL.
+            ENDPOINT_URL: preprod.urlOutput,
+          },
+        }),
+  
+      ],
+    });
   }
 }
